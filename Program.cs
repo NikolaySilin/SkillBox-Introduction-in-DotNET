aae4856afb80b50f090b1524ba50d78ef605f8d7
Nikolay Silin <silinnikolay@yandex.ru>
2022-01-27 20:26:21 +0500

    task 3 completed

--- Program.cs
+++ Program.cs
@@ -11,23 +11,16 @@ namespace Homework_01
         static void Main(string[] args)
         {
             // Создание базы данных из 20 сотрудников
-            Repository repository = new Repository(30);
+            Repository repository = new Repository(50);
 
             // Печать в консоль всех сотрудников
             repository.Print("База данных до преобразования");
 
-            // Увольнение всех работников с именем "Агата"
-            repository.DeleteWorkerByName("Агата");
+            // Увольнение всех работников чья зарплата превышает 30000руб
+            repository.DeleteWorkerBySalary(30000);
 
             // Печать в консоль сотрудников, которые не попали под увольнение
             repository.Print("База данных после первого преобразования");
-
-            // Увольнение всех работников с именем "Аделина"
-            repository.DeleteWorkerByName("Аделина");
-
-            // Печать в консоль сотрудников, которые не попали под увольнение
-            repository.Print("База данных после второго преобразования");
-
 
             #region Домашнее задание
 
